c3a375b9680c7d4b8a3bc6041ca889b42c24e139
Louka <loukagaspari@hotmail.com>
2021-07-30 09:05:51 +0200

    Initialisation ClientReservationJFrame

--- src/VIEW/ClientReservationSpectacleJFrame.java
+++ src/VIEW/ClientReservationSpectacleJFrame.java
@@ -32,7 +32,7 @@ public class ClientReservationSpectacleJFrame extends JFrame {
 	private JList<Representation> listRepresentations;
 	/**
 	 * Launch the application.
-	 */
+	 **/
 	public static void main(String[] args) 
 	{
 		EventQueue.invokeLater(new Runnable() 
